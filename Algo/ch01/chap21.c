8d89f990ac2ee26d0fe4a61004ef729ad102ab7e
jaesoon-park <wotns2606@cu.ac.kr>
2019-09-16 13:20:55 +0900

    algorithm

---
+++ Algo/ch01/chap21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+	printf("          =========°ö¼ÀÇ¥=========\n");
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			printf(" %3d ", i * j);
+		}
+		printf("\n");
+	}
+}
