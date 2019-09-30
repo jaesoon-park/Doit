359b246ccd276c8f126f6a9aad2d963ac91a8cea
jaesoon-park <wotns2606@cu.ac.kr>
2019-09-30 14:13:56 +0900

    chap132

--- Algo/ch03/chap109.c
+++ Algo/ch03/chap109.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define _PJS
 
 #ifdef _PJS
 
---
+++ Algo/ch04/intStack.h
@@ -0,0 +1,26 @@
+#pragma once
+#define _PJS
+#ifdef _PJS
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+typedef struct {
+	int max;
+	int ptr;
+	int *stk;
+}IntStack;
+int Initialize(IntStack *s, int max);//스택초기화
+int Push(IntStack *s, int x);//스택에 데이터를 푸시
+int Pop(IntStack *s, int *x);//스택에서 데이터를 팝
+int Peek(const IntStack *s, int *x);// 스택에서 데이터를 피크
+void Clear(IntStack *s);//스택 비우기
+int Capacity(const IntStack *s);//스택의 최대용량
+int Size(const IntStack *s);//스택의 데이터 개수
+int IsEmpty(const IntStack *s);//스택이 비어 있나요?
+int IsFull(const IntStack *s);//스택이 가득찼나요?
+int Search(const IntStack *s, int x);//스태에서 검색
+void Print(const IntStack *s);//모든데이터 출력
+void Terminate(IntStack *s);// 스택종료 
+	
+
+
+#endif
