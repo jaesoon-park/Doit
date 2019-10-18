6df1565a80ccdf5aa409909486f5993fbfb05a42
jaesoon-park <wotns2606@cu.ac.kr>
2019-10-18 11:47:24 +0900

    20191018

--- Algo/chap06/ch230.c
+++ Algo/chap06/ch230.c
@@ -6,45 +6,47 @@
 
 
 #ifdef _PJS
-void insertion(int a[], int n)
+void quick(int a[],int left, int right)
 {
+	
+	int pl = left;
+	int pr = right;
+	int x = a[(pl + pr) /2];
 
-	int i,j;
-	
-	for (i = 1; i < n; i++) {
-		int tmp = a[i];
-		for (j = i; j > 0 && a[j - 1] > tmp; j--) {
-			a[j] = a[j - 1];
+	do {
+		while (a[pl] < x)pl++;
+		while (a[pr] > x)pr--;
+		if (pl <= pr) {
+			swap(int, a[pl], a[pr]);
+			pl++;
+			pr--;
 		}
-		a[j] = tmp;
-	}
+	} while (pl <= pr);
 	
+	if (left < pr) quick(a, left, pr);
+	if (pl < right) quick(a, pl, right);
+
 }
 
-int main(void)
+int main()
 {
 	int i, nx;
 	int *x;
-
-	puts("단순 삽입 정렬");
+	puts("퀵정렬");
 	printf("요소 개수 : ");
 	scanf("%d", &nx);
 	x = calloc(nx, sizeof(int));
-
 	for (i = 0; i < nx; i++) {
 		printf("x[%d] : ", i);
 		scanf("%d", &x[i]);
 	}
 
-	insertion(x, nx);
 
+	quick(x, 0, nx - 1);
 	puts("오름차순으로 정렬했습니다.");
 	for (i = 0; i < nx; i++)
 		printf("x[%d] = %d\n", i, x[i]);
-
 	free(x);
-
-	return 0;
 }
 
 #endif
